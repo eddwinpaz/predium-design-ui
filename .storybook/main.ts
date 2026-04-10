91a0e71c19f45c5aa31c71ee357936c496defa48
Eddwin Paz <pazeddwin@gmail.com>
2026-04-10 00:47:39 -0400

    Remove storybook addon-vitest and unused addons causing startup error

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,11 +6,8 @@ const config: StorybookConfig = {
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-vitest",
     "@storybook/addon-a11y",
-    "@storybook/addon-docs",
-    "@storybook/addon-onboarding"
+    "@storybook/addon-docs"
   ],
   "framework": "@storybook/vue3-vite"
 };
